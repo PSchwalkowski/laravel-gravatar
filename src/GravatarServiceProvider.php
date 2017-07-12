e1c29f23ef47cfef0b42162cda0d595481731125
Paweł <pawel.schwalkowski@kbsystems.pl>
2017-07-12 12:25:59 +0200

    Initial commit

---
+++ src/GravatarServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PS\Gravatar;
+
+use Illuminate\Support\ServiceProvider;
+
+class GravatarServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		
+	}
+}
