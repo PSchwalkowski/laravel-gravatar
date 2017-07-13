6fd6b891609260140fee9f5add84b1546a036637
Paweł <p.schwalkowski@gmail.com>
2017-07-13 19:23:38 +0200

    Gravatar static class

---
+++ src/Gravatar.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace PS\Gravatar;
+
+class Gravatar {
+	
+	/**
+	 * Url to gravatar generator
+	 *
+	 * @var string
+	 */
+	protected static $gravatarUrl = 'https://www.gravatar.com/avatar/';
+
+	public static function generate(string $email, array $options = []) {
+
+		if (!self::validateEmail($email)) 
+			return false;
+
+		$hash = self::createHashFromEmail($email);
+		$url = self::makeGravatarUrl($hash, $options);
+
+		return $url;
+	}
+
+	/**
+	 * Validate given email address
+	 *
+	 * @param string $email
+	 * @return bool
+	 */
+	protected static function validateEmail(string $email): bool {
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+
+	/**
+	 * Create md5 hash from email adress
+	 * @see https://pl.gravatar.com/site/implement/hash/
+	 *
+	 * @param string $email
+	 * @return string
+	 */
+	protected static function createHashFromEmail(string $email): string {
+		return md5(strtolower(trim($email)));
+	}
+
+	protected static function makeGravatarUrl(string $hash, array $options): string {
+		$url = self::$gravatarUrl . $hash;
+		if (!empty($options)) {
+			$url .= '?';
+
+			foreach ($options as $key => $value) {
+				$url .= $key . '=' . $value;
+			}
+		}
+
+		return $url;
+	}
+}
